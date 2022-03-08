ea70b1b2c4947b004cf389ca9bca8d818b4bd2f6
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2022-03-08 14:00:22 -0300

    update port

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
 
 
 
-/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
- */
+
